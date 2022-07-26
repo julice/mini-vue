a03b93508dc635c1aaef7466bdbced5902f6be96
fangliji <fangliji@shencom.cn>
2022-07-26 21:12:49 +0800

    新增readonly，以及reactive优化

---
+++ src/reactivity/basezHandlers.ts
@@ -0,0 +1,37 @@
+import { track, trigger } from './effect';
+
+const get = createGetter();
+const set = createSetter();
+const readonlyget = createGetter(true);
+
+function createGetter(readonly = true) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    // todo 依赖收集
+    track(target, key);
+    return res;
+  };
+}
+function createSetter(isReadonly = false) {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // 触发依赖
+    if (!isReadonly) {
+      trigger(target, key);
+    }
+    return res;
+  };
+}
+
+export const mutableHandlers = {
+  get,
+  set,
+};
+
+export const readonlyHandlers = {
+  get: readonlyget,
+  set(target, key, value) {
+    console.warn('it is readonly obj');
+    return true;
+  },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,18 +1,15 @@
+import { mutableHandlers, readonlyHandlers } from './basezHandlers';
 import { track, trigger } from './effect';
 
+function createActiveObject(raw, handler) {
+  return new Proxy(raw, handler);
+}
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // todo 依赖收集
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      // 触发依赖
-      trigger(target, key);
-      return res;
-    },
-  });
+  return createActiveObject(raw, mutableHandlers);
+}
+
+export function readonly(raw) {
+  {
+    return createActiveObject(raw, readonlyHandlers);
+  }
 }
